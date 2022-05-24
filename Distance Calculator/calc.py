3b916786472f4ce43ae031f096d7a618ebb02e77
Anjita Bhandari <88409698+abhand3@users.noreply.github.com>
2022-05-23 23:54:00 -0400

    Update calc.py
    with some updates

--- Distance Calculator/calc.py
+++ Distance Calculator/calc.py
@@ -1,5 +1,32 @@
 import RPi.GPIO as GPIO
 import time
+import smtplib
+
+SMTP_SERVER = 'smtp.gmail.com' #Email Server (don't change!)
+SMTP_PORT = 587 #Server Port (don't change!)
+GMAIL_USERNAME ='' //your email
+GMAIL_PASSWORD = '' //password
+
+#Create class to send an email
+class Emailer:
+	def sendmail(self, recipient,  subject, content):
+		#Creating the headers
+		headers = ["From: " + GMAIL_USERNAME, "Subject: " +subject, 
+			"To: " + recipient, "MIME-Version 1.0", "Content-Type: text/html"]
+		headers = "\r\n".join(headers)
+
+		#Connect to Gmail Server
+		session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+		session.ehlo()
+		session.starttls()
+		session.ehlo()
+
+		#Login to Gmail
+		session.login(GMAIL_USERNAME, GMAIL_PASSWORD)
+
+		#Send Email & Exit
+		session.sendmail(GMAIL_USERNAME, recipient, headers + "\r\n\r\n" + content)
+		session.quit
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -17,6 +44,11 @@ time.sleep(2)
 print ("Place the object......")
 
 object = True
+pulse_start1 = time.time()
+pulse_end1 = time.time()
+pulse_duration1 = pulse_end1 - pulse_start1
+distance1 = pulse_duration1 * 17150
+distance1 = round(distance1+1.15, 1)
 
 try:
     while object:
@@ -42,6 +74,14 @@ try:
           print ("distance:",distance,"cm")
           i=1
           dist2 = distance
+          if(distance!=distance1):
+            sender = Emailer()
+            sendTo = '' //email
+            emailSubject = "Distance Calculator"
+            emailContent = "Distance is" + str(distance)
+
+            sender.sendmail(sendTo, emailSubject, emailContent)
+            distance1= distance
           
        if distance>20 and i==1:
           print ("place the object....")
